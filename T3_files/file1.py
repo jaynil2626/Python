8048999da410c57940884d7148fd70c34594c30f
Jaynil_26 <ompatell2006@gmail.com>
2024-12-07 03:58:39 +0000

    add

--- T3_files/file1.py
+++ T3_files/file1.py
@@ -1,1 +1,9 @@
-self_object=open('self.txt','r')
+# File Opening
+self=open('self.txt','r')
+print(self)
+self.close() # Close File to free RAM
+
+# File Opening From Path (Other Directory)
+f=open('/workspaces/Python/T2_str_list_dict/abc.txt','r')
+print(f)
+f.close() # Close File
