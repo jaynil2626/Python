0e1a6fb6d56b88871085a8ad20f5e533cdadc303
Jaynil_26 <ompatell2006@gmail.com>
2024-12-18 05:09:45 +0000

    add

--- OOP/1class.py
+++ OOP/1class.py
@@ -1,14 +1,35 @@
-class lj:
-    def __init__(self,name,grade):
-        self.name=name
-        self.grade=grade
-        print(id(self))
+# class lj:
+#     def __init__(self,name,grade):
+#         self.name=name
+#         self.grade=grade
     
-    def set(number):
-        print(number)
+#     def set(self,number):
+#         print(number)
 
-s1=lj('Vishal',10)
-print(id(s1))
-s1.set(30)
-s2=lj('vha',9)
-print(id(s2))
+# s1=lj('V',10)
+# print(s1.__dict__)
+# s2=lj('H',9)
+# print(s2.__dict__)
+# s1.name='Vishal'
+# print(s1.__dict__)
+
+class vishal:
+    movie='pushpa'
+    def __init__(self,bud,rev):
+        self.bud=bud
+        self.rev=rev
+    
+    def set(self):
+        print(self.movie)
+
+o=vishal(10,20)
+print(o.movie)
+print(o.bud)
+o.bud=50
+o.movie='pushpa2'
+print(o.movie)
+print(o.bud)
+print(o.__dict__)
+print(vishal.movie)
+vishal.movie='pushpa2'
+print(vishal.movie)
