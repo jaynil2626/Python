dcd163786e4491b8013017a7f9811f181ccba890
Jaynil_26 <ompatell2006@gmail.com>
2024-12-24 06:08:00 +0000

    add

--- Modulee/Acalc.py
+++
@@ -1,6 +0,0 @@
-import Bcalci_program as c
-
-print(c.add(1,2))
-print(c.multi(2,3))
-print(c.div(4,2))
-print(c.sub(3,2))
--- Modulee/Astr.py
+++
@@ -1,4 +0,0 @@
-import Bstr_program as a
-
-print(a.find('helloworldhowareyou','are'))
-print(a.rfind('helloworldhowareyou','area'))
